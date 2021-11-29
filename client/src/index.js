46a1f1fc353ef2c17e169e2754ab9c1bc000101c
Jesus Salazar <jase036@gmail.com>
2021-11-29 12:59:24 -0700

    Added Auth0 dependencies and provider + set up the app settings in Auth0 panel

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain="dev-3v3jf16o.us.auth0.com"
+    clientId="DwbBaIe7YnyJStRuq7XcsDx6pfLUu1fz"
+    redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
