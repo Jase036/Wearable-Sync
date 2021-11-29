d77f2835d859d7026288c18db60e83e223dc2c60
Jesus Salazar <jase036@gmail.com>
2021-11-29 16:05:06 -0700

    Clean-up and added pagination to handlers

--- server/handlers.js
+++ server/handlers.js
@@ -2,48 +2,82 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
-const assert = require("assert");
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
+
+//Declare our MongoDB client and options along with the database name so we don't have to repeat in all handler functions.
 const client = new MongoClient(MONGO_URI, options);
 const db = client.db("Ecommerce");
-// function to get all companies from mongodb collection companies
+
+
+
 const getAllCompanies = async (req, res) => {
+  
+  //To paginate from server we use skip & limit as query parameters. In case they aren't sent, we default to skip 0 and limit 20.
+  let {skip, limit} = req.query
+  skip? skip = Number(skip) : skip = 0;
+  limit? limit = Number(limit) : limit = 20;
+
   try {
     await client.connect();
     const companiesList = await db.collection("companies").find().toArray();
-    // console.log(companiesList);
-    res.status(200).json({ status: 200, data: companiesList });
-  } catch (err) {
-    res.status(404).json({ status: 404, err: err.stack });
-  } finally {
+    
     client.close();
-  }
+
+    if (companiesList.length !== 0) {
+      res.status(200).json({ status: 200, data: productsList });
+      } else {
+        res.status(404).json({ status: 404, error: err.message });
+      }
+
+    } catch (err) {
+      res.status(500).json({ status: 500, error: err.message });
+    }
+  
 };
+
+
 const getAllProducts = async (req, res) => {
+  
+  //To paginate from server we use skip & limit as query parameters. In case they aren't sent, we default to skip 0 and limit 20.
+  let {skip, limit} = req.query
+  skip? skip = Number(skip) : skip = 0;
+  limit? limit = Number(limit) : limit = 20;
+
   try {
+    
     await client.connect();
-    const productsList = await db.collection("items").find().toArray();
-    console.log("connect")
-    // console.log(productsList);
-    res.status(200).json({ status: 200, data: productsList });
-  } catch (err) {
-    res.status(404).json({ status: 404, error: err.stack });
-  } finally {
+    
+    const productsList = await db.collection("items").find().skip(skip).limit(limit).toArray();
+    
     client.close();
+
+    if (productsList.length !== 0) {
+    res.status(200).json({ status: 200, data: productsList });
+    } else {
+      res.status(404).json({ status: 404, error: err.message });
+    }
+
+  } catch (err) {
+    res.status(500).json({ status: 500, error: err.message });
   }
 };
+
+
 const getCompanyById = async (req, res) => {
+  
+  //Transform _id string to number so we can use it to search for company _id
   const _id = Number(req.params._id);
+  
   try {
     await client.connect();
-    const companyById = await db
-      .collection("companies")
-      .find({ _id })
-      .toArray();
-    console.log(companyById);
+    const companyById = await db.collection("companies").findOne({ _id });
+    
+    client.close();
+
     if (companyById) {
       res.status(200).json({ status: 200, data: companyById });
     } else {
@@ -53,17 +87,21 @@ const getCompanyById = async (req, res) => {
     }
   } catch (err) {
     console.log(err.message);
-    res.status(500).json({ status: 500, error: err.stack });
-  } finally {
-    client.close();
+    res.status(500).json({ status: 500, error: err.message });
   }
 };
+
+
 const getProductById = async (req, res) => {
+  
+  //Transform _id string to number so we can use it to search for product _id
   const _id = Number(req.params._id);
+
   try {
     await client.connect();
     const productById = await db.collection("items").findOne({ _id });
-    console.log(productById);
+    client.close();
+
     if (productById) {
       res.status(200).json({ status: 200, data: productById });
     } else {
@@ -72,11 +110,11 @@ const getProductById = async (req, res) => {
         .json({ status: 404, message: "The product Id is not found" });
     }
   } catch (err) {
-    res.status(500).json({ status: 500, error: err.stack });
-  } finally {
-    client.close();
+    res.status(500).json({ status: 500, error: err.message });
   }
 };
+
+
 const getProductsByCategory = async (req, res) => {
   const category = req.params.category;
   console.log(typeof category);
@@ -86,6 +124,9 @@ const getProductsByCategory = async (req, res) => {
       .collection("items")
       .find({ category: category })
       .toArray();
+      
+    client.close();
+
     if (products) {
       res.status(200).json({ status: 200, data: products });
     } else {
@@ -94,11 +135,11 @@ const getProductsByCategory = async (req, res) => {
         .json({ status: 404, message: "The product category is not found" });
     }
   } catch (err) {
-    res.status(500).json({ status: 500, error: err.stack });
-  } finally {
-    client.close();
+    res.status(500).json({ status: 500, error: err.message });
   }
 };
+
+
 module.exports = {
   getAllCompanies,
   getAllProducts,
