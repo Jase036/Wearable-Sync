3426d20594c8389d675cae804665016bc1547ca5
Madhoo14 <89360388+Madhoo14@users.noreply.github.com>
2021-12-01 23:53:52 -0500

    searchbar error fix

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -2,16 +2,17 @@ import React from "react";
 import Header from "./Header";
 import CatalogRender from "./CatalogRender";
 import Footer from "./Footer";
+import SearchError from "./Navbar/SearchError";
 
 //styling
 
 const Search = () => {
   return (
-    <>
+    <div>
       <Header />
       <CatalogRender />
       <Footer />
-    </>
+    </div>
   );
 };
 
