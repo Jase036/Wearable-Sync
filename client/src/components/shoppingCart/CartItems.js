0d5e6016aee5649f631b9c9e887f5b1093ec351a
Irina Cucereavii <irina.cucereavii@gmail.com>
2021-12-02 01:54:53 -0500

    shopping cart

--- client/src/components/shoppingCart/CartItems.js
+++ client/src/components/shoppingCart/CartItems.js
@@ -6,19 +6,31 @@ import styled from "styled-components";
 
 
 
-const CartItems = ({item}) => {
+const CartItems = ({item, cart}) => {
+    const {state, addQuantity, removePurchase, lowerQuantity} = useContext(ItemContext)
 
-    const {state, addPurchase, removePurchase, clearPurchase} = useContext(ItemContext)
+    let cartInfo = [];
+    if(state.hasLoaded) {
+        cart.forEach((elem) => {
+            if (elem.product_id === item._id) {
+                cartInfo.push(elem);
+            }
+        })
+    }
+
+    let disabledLower = cartInfo[0].quantity === 1 ? true : false;
+    let disabledAdd = cartInfo[0].quantity < item.numInStock ? false : true;
+
 
     return (
         <div>
                 <React.Fragment key="item._id">
-                    <button onClick={() => clearPurchase(item._id)}>Remove</button>
-                    <img src={item.imageSrc} alt={item.name}/>
+                    <button onClick={() => removePurchase(cartInfo)}>Remove</button>
+                    <StyledImg src={item.imageSrc} alt={item.name}/>
                     <p>{item.name}</p>
-                    <p>{item.quantity}</p>
-                    <button onClick={() => addPurchase(item._id)}> + </button>
-                    <button onClick={() =>removePurchase(item._id)}> - </button>
+                    <p>{cartInfo[0]?.quantity}</p>
+                    <button onClick={() => addQuantity(cartInfo)} disabled={disabledAdd} > + </button>
+                    <button onClick={() => lowerQuantity(cartInfo)} disabled={disabledLower}> - </button>
                     <p>{item.price}</p>
                 </React.Fragment>
                 
@@ -26,5 +38,13 @@ const CartItems = ({item}) => {
         </div>
     )
 }
+
+
+const StyledImg = styled.img`
+width: 65px;
+height: 65px;
+mix-blend-mode: multiply;
+`;
+
 
 export default CartItems
