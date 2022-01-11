250f4f45dd73264dcf852d4cb29c6dca1a644989
Jesus Salazar <jase036@gmail.com>
2022-01-11 11:18:36 -0700

    env file

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { ItemProvider } from "./components/ItemContext";
 ReactDOM.render(
   <Auth0Provider
     domain="dev-3v3jf16o.us.auth0.com"
-    clientId="DwbBaIe7YnyJStRuq7XcsDx6pfLUu1fz"
+    clientId={process.env.REACT_APP_AUTH0_ID}
     redirectUri={window.location.origin}>
     <ItemProvider>
       <App />
